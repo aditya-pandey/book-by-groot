4ef16b18dc73554033d025187ef76870bfabf945
Aditya <39548065+aditya-pandey@users.noreply.github.com>
2020-04-07 20:20:45 +0530

    Update book.js

--- Book/book.js
+++ Book/book.js
@@ -2,11 +2,11 @@ var indexI =document.querySelector(".index-i");
 var index = document.querySelector(".index");
 
 indexI.addEventListener("click", () => {
-    if(index.style.display == "none"){
-        index.style.display = "flex";
+    if(index.style.display == "flex"){
+        index.style.display = "none";
     }
     else{
-        index.style.display = "none";
+        index.style.display = "flex";
     }
     ;
 })
