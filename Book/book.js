e38779eb2bc72da3682295fc216ca31b9c79404c
Aditya <39548065+aditya-pandey@users.noreply.github.com>
2020-04-13 18:58:48 +0530

    Add files via upload

--- Book/book.js
+++ Book/book.js
@@ -1,12 +1,33 @@
 var indexI =document.querySelector(".index-i");
+var fontI = document.querySelector(".font-i");
+var themeI = document.querySelector(".theme-i");
 var index = document.querySelector(".index");
+var fontProp = document.querySelector(".font-prop");
+var theme = document.querySelector(".theme");
+var themechanger = document.getElementsByName("theme");
 
-indexI.addEventListener("click", () => {
-    if(index.style.display == "flex"){
-        index.style.display = "none";
-    }
-    else{
-        index.style.display = "flex";
-    }
-    ;
+var font = document.querySelector("#fonts");
+var paper = document.querySelectorAll(".paper p");
+
+var icons = [indexI,fontI,themeI];
+var features = [index,fontProp,theme];
+
+for(let i=0;i<icons.length;i++){
+    icons[i].addEventListener("click", () => {
+        if(features[i].style.display == "flex"){
+            features[i].style.display = "none";
+        }
+        else{
+            features[i].style.display = "flex";
+        }
+        icons.filter((a) => {if(a != icons[i]){
+            console.log(i)
+        }} );
+        ;
+    })
+}
+font.addEventListener("change",() => {
+    paper.forEach(el => {
+        el.style.fontFamily = `${font.value}`;
+    });
 })
